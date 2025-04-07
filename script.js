98400819125110b7f293d9c5c99b22841826e3a0
DominiqueBullard <dominique.miller21@gmail.com>
2025-04-07 22:11:51 +0000

    fixes

---
+++ script.js
@@ -0,0 +1,16 @@
+const title = document.getElementById("title");
+console.log(title);
+const pTag = document.querySelector ("p");
+const aTag = document.querySelector ("a");
+const hTag = document.querySelector("h2");
+
+const h2Tag= document.querySelectorAll("h2");
+
+const p2Tag = document.getElementById("mew mew mew");
+p2Tag.innerText = "meow";
+
+const image = document.querySelector("#myImage")
+.scr ="lanz.png";
+
+const div = document.createElement("div");
+div.innerText = "blah"
